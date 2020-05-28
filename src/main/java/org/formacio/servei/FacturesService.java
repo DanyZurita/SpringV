a285c8895a8f2deb53eaa0cfa00486ab10fccca8
DanyZurita <dzurita@cifpfbmoll.eu>
2020-05-28 15:32:54 +0200

    feat(FacturesServei): Añadir notificacion al cliente usando fidelService.notificaRegal() con logica para caso test

--- src/main/java/org/formacio/servei/FacturesService.java
+++ src/main/java/org/formacio/servei/FacturesService.java
@@ -24,6 +24,8 @@ public class FacturesService {
 	@Autowired
 	FacturesRepositori repoFacts;
 	
+	@Autowired
+	FidalitzacioService fidelService;
 	
 	
 	public Factura afegirProducte (long idFactura, String producte, int totalProducte) {
@@ -37,6 +39,11 @@ public class FacturesService {
 			fact.get().getLinies().add(linia);
 			repoFacts.save(fact.get());
 			
+			//Cada 4 lineas en la factura de comunica al cliente por el email que tiene un regalo
+			if (fact.get().getLinies().size() >= 4) {
+				String email = fact.get().getClient().getEmail();
+				fidelService.notificaRegal(email);
+			}
 			return fact.get();
 		}
 		else {
