359b6d0ab7664f7240c264295fd6eed5375c9b0b
DanyZurita <dzurita@cifpfbmoll.eu>
2020-05-28 15:28:45 +0200

    feat(FacturesServei): Implementar afegirProducte() con repoFacts y Transactional

--- src/main/java/org/formacio/servei/FacturesService.java
+++ src/main/java/org/formacio/servei/FacturesService.java
@@ -1,10 +1,18 @@
 package org.formacio.servei;
 
-import org.formacio.domain.Factura;
+import java.util.Optional;
 
+import org.formacio.domain.Factura;
+import org.formacio.domain.LiniaFactura;
+import org.formacio.repositori.FacturesRepositori;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class FacturesService {
 
-	
 	/*
 	 * Aquest metode ha de carregar la factura amb id idFactura i afegir una nova linia amb les dades
 	 * passades (producte i totalProducte)
@@ -13,8 +21,27 @@ public class FacturesService {
 	 * 
 	 * Per implementar aquest metode necessitareu una referencia (dependencia) a FacturesRepositori
 	 */
+	@Autowired
+	FacturesRepositori repoFacts;
+	
+	
+	
 	public Factura afegirProducte (long idFactura, String producte, int totalProducte) {
 		
-		return null;
+		Optional<Factura> fact = repoFacts.findById(idFactura);
+		LiniaFactura linia = new LiniaFactura();
+		
+		if (fact.isPresent()) {
+			linia.setProducte(producte);
+			linia.setTotal(totalProducte);
+			fact.get().getLinies().add(linia);
+			repoFacts.save(fact.get());
+			
+			return fact.get();
+		}
+		else {
+			return null;
+		}
+		
 	}
 }
